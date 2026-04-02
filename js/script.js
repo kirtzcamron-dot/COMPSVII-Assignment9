be4be34e2a601edff01a39e70081c1f15321a4c7
Kirtz Camron <kirtz.camron@gmail.com>
2026-04-01 21:18:37 -0400

    commit -m "Added functionality to toggle the visibility of the navigation menu when the hamburger icon is clicked."

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("script.js loaded");
+const apiKey = "YOUR_API_KEY_HERE";
+
+let endpoint = `https://api.giphy.com/v1/gifs/search?q=cats&api_key=${apiKey}&limit=10`;
+
+async function fetchGifs() {
+    let response = await fetch(endpoint);
+    let data = await response.json();
+
+    let images = data.data.map(gif => gif.images.original.url);
+
+    console.log(images); // check your data
+    return images;
+}
+
+
